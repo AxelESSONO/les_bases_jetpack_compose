0bfd25b7ad5749d7e75b883715081110ff9ac970
axel-bis <naxelcdric@gmail.com>
2025-04-23 17:46:35 +0200

    constraint_layout.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +58,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
 }
--- app/src/main/java/com/eseo/lesbasesjetpackcompose/layouts/constraint_layout.kt
+++ app/src/main/java/com/eseo/lesbasesjetpackcompose/layouts/constraint_layout.kt
@@ -1,2 +1,45 @@
 package com.eseo.lesbasesjetpackcompose.layouts
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.eseo.lesbasesjetpackcompose.ui.theme.Purple40
+
+@Preview
+@Composable
+fun MyConstraintLayout(modifier: Modifier = Modifier) {
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Purple40)
+    ) {
+
+        val(box1, box2) = createRefs()
+
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .background(color = Color.Red)
+                .constrainAs(box1) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .background(color = Color.Magenta)
+        )
+
+    }
+}
